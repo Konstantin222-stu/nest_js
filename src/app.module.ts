3f94f88e914853fff893dce943643643a383fcc8
Константин <konstantinzadoro@gmail.com>
2025-06-13 15:01:08 +0300

    connection database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [UsersController],
+    providers: [],
+    imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'QAZWSXEDC',
+      database: 'nestjs',
+    autoLoadModels: true,
+      models: [],
+    }),  
+        UsersModule
+    ],
 })
 export class AppModule{}
